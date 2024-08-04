80dcd875f2ff789a3bd0eadd7549807f0b9c763e
Hewdraw <hewdraw@live.nl>
2024-08-04 18:59:49 +0200

    tera blast partially

--- Data/Scripts/011_Battle/002_Move/007_Move_Effects_100-17F.rb
+++ Data/Scripts/011_Battle/002_Move/007_Move_Effects_100-17F.rb
@@ -2515,3 +2515,18 @@ class PokeBattle_Move_176 < PokeBattle_Move
     end
   end
 end
+
+class PokeBattle_Move_177 < PokeBattle_Move
+  def pbGetAttackStats(user, target)
+    stageMul = [2,2,2,2,2,2, 2, 3,4,5,6,7,8]
+    stageDiv = [8,7,6,5,4,3, 2, 2,2,2,2,2,2]
+    attackstage = user.stages[:ATTACK] + 6
+    attack = user.attack*stageMul[attackstage]/stageDiv[attackstage]
+    spatkstage = user.stages[:SPECIAL_ATTACK] + 6
+    spatk = user.spatk*stageMul[spatkstage]/stageDiv[spatkstage]
+    if attack > spatk
+      return user.attack, user.stages[:ATTACK] + 6
+    end
+    return user.spatk, user.stages[:SPECIAL_ATTACK] + 6
+  end
+end
