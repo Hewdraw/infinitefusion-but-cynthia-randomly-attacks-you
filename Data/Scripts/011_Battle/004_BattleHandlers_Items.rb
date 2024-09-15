93f9008d842098693be0bd77c1a3fd92f0ff1586
Hewdraw <hewdraw@live.nl>
2024-09-15 23:05:38 +0200

    consume throat spray

--- Data/Scripts/011_Battle/004_BattleHandlers_Items.rb
+++ Data/Scripts/011_Battle/004_BattleHandlers_Items.rb
@@ -1276,7 +1276,8 @@ BattleHandlers::UserItemAfterMoveUse.add(:THROATSPRAY,
     next false if !user.pbCanRaiseStatStage?(:SPECIAL_ATTACK,user)
     itemName = GameData::Item.get(item).name
     battle.pbCommonAnimation("UseItem",user)
-    next user.pbRaiseStatStageByCause(:SPECIAL_ATTACK,1,user,itemName)
+    user.pbRaiseStatStageByCause(:SPECIAL_ATTACK,1,user,itemName)
+    next true
   }
 )
 
