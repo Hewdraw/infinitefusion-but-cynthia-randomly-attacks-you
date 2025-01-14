861bcb1df5bb6687c8dc03ca78044d29841fc4ce
Hewdraw <hewdraw@live.nl>
2025-01-14 11:21:36 +0100

    giovanni music

--- Data/Scripts/019_Utilities/003_Utilities_BattleAudio.rb
+++ Data/Scripts/019_Utilities/003_Utilities_BattleAudio.rb
@@ -124,6 +124,9 @@ def pbGetTrainerBattleBGM(trainer)   # can be a Player, NPCTrainer or an array o
     if trainer_type_data.id == :TEAMROCKET
       return pbStringToAudioFile("Dennis")
     end
+    if trainer_type_data.id == :LEADER_Giovanni
+      return pbStringToAudioFile("VSGiovanni")
+    end
   end
   ret = pbStringToAudioFile(music) if music && music!=""
   if !ret
