266838bb754ab33fe18271f184d3a7edcca48880
Hewdraw <hewdraw@live.nl>
2025-09-22 15:57:24 +0200

    Update 002_Battler_Initialize.rb

--- Data/Scripts/011_Battle/001_Battler/002_Battler_Initialize.rb
+++ Data/Scripts/011_Battle/001_Battler/002_Battler_Initialize.rb
@@ -294,7 +294,7 @@ class PokeBattle_Battler
     @effects[PBEffects::Protosynthesis]                = 0
     @effects[PBEffects::QuarkDrive]                = 0
     @effects[PBEffects::SaltCure]                = 0
-    @effects[PBEffects::RageFist]                = 1 if @effects[PBEffects::RageFist] == ni
+    @effects[PBEffects::RageFist]                = 1 if @effects[PBEffects::RageFist] == nil
   end
 
   #=============================================================================
