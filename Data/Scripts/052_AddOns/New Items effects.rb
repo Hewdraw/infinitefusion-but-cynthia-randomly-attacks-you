5b8d96447e4cfc6669c2f94b81f861cf937abb48
Hewdraw <hewdraw@live.nl>
2025-10-07 23:56:37 +0200

    Update New Items effects.rb

--- Data/Scripts/052_AddOns/New Items effects.rb
+++ Data/Scripts/052_AddOns/New Items effects.rb
@@ -1150,7 +1150,7 @@ ItemHandlers::UseOnPokemon.add(:SINNOHCOIN, proc { |item, pkmn, scene|
     level = pkmn.level
     pkmn.species = :WROTOM
     pkmn.level = level
-    next false
+    next true
   end
   if pkmn.isSpecies?(:ROTOM)
     level = pkmn.level
