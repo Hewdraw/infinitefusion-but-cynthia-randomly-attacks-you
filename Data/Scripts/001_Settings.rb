1db88d065652034c1fb05da68d73b72a1cf4f4aa
Hewdraw <hewdraw@live.nl>
2026-01-02 21:10:27 +0100

    lati music

--- Data/Scripts/001_Settings.rb
+++ Data/Scripts/001_Settings.rb
@@ -421,7 +421,7 @@ module Settings
     [:ENTEI, 50, 350, 1, "Legendary Birds",ROAMING_AREAS,:Sunny],
     [:B245H243, 50, 341, 1, "Legendary Birds",ROAMING_AREAS,:Storm],
     #[:B379H378, 50, 602, 0, "Legendary Birds",SEVII_ROAMING,:StrongWinds],
-    [:B378H379, 50, 602, 0, "Legendary Birds",SEVII_ROAMING,:StrongWinds],
+    [:B378H379, 50, 602, 0, "VSLati",SEVII_ROAMING,:StrongWinds],
     [:FEEBAS, 15, 4, 3, "Pokemon HeartGold and SoulSilver - Wild Pokemon Battle (Kanto)",SEVII_ROAMING,:Rain]
   ]
 
