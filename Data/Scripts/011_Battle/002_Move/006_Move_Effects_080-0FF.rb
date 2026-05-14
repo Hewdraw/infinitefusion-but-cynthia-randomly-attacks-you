1d9e00ab84a4ca77eeaf8ba4438fe651ba3a4b79
Hewdraw <hewdraw@live.nl>
2026-05-14 18:06:53 +0200

    Update 006_Move_Effects_080-0FF.rb

--- Data/Scripts/011_Battle/002_Move/006_Move_Effects_080-0FF.rb
+++ Data/Scripts/011_Battle/002_Move/006_Move_Effects_080-0FF.rb
@@ -250,6 +250,19 @@ class PokeBattle_Move_090 < PokeBattle_Move
   def pbBaseDamage(baseDmg,user,target)
     return super
   end
+
+  def pbGetAttackStats(user, target)
+    stageMul = [2,2,2,2,2,2, 2, 3,4,5,6,7,8]
+    stageDiv = [8,7,6,5,4,3, 2, 2,2,2,2,2,2]
+    attackstage = user.stages[:ATTACK] + 6
+    attack = user.attack*stageMul[attackstage]/stageDiv[attackstage]
+    spatkstage = user.stages[:SPECIAL_ATTACK] + 6
+    spatk = user.spatk*stageMul[spatkstage]/stageDiv[spatkstage]
+    if attack > spatk
+      return user.attack, user.stages[:ATTACK] + 6
+    end
+    return user.spatk, user.stages[:SPECIAL_ATTACK] + 6
+  end
 end
 
 
