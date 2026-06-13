dbbed0e21258b280d989f0541719c44f54a0a3da
Hewdraw <hewdraw@live.nl>
2026-06-13 21:37:36 +0200

    lass offset

--- Data/Scripts/011_Battle/004_AI/006_AI_Move_Utilities.rb
+++ Data/Scripts/011_Battle/004_AI/006_AI_Move_Utilities.rb
@@ -127,7 +127,7 @@ class PokeBattle_AI
       when :ICE
         return true if target.hasActiveAbility?(:POLAR)
       end
-      return true if Effectiveness.not_very_effective?(typeMod) &&
+      return true if typeMod < 4 &&
                      target.hasActiveAbility?(:WONDERGUARD)
       return true if move.damagingMove? && user.index!=target.index && !target.opposes?(user) &&
                      target.hasActiveAbility?(:TELEPATHY)
