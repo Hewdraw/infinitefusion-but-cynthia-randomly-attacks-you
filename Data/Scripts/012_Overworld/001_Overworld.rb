ad964faf2e55c299abb2140901e2409e85d5a680
Hewdraw <hewdraw@live.nl>
2026-08-16 17:26:00 +0200

    Update 001_Overworld.rb

--- Data/Scripts/012_Overworld/001_Overworld.rb
+++ Data/Scripts/012_Overworld/001_Overworld.rb
@@ -639,7 +639,7 @@ def pbLedge(_xOffset, _yOffset)
     if pbJumpToward(2, true)
       $scene.spriteset.addUserAnimation(Settings::DUST_ANIMATION_ID, $game_player.x, $game_player.y, true, 1)
       $game_player.increase_steps
-      pbSEPlay("OOF") if rand(10) == 0
+      pbSEPlay("OOF") if rand(6) == 0
       $game_player.check_event_trigger_here([1, 2])
     end
     return true
