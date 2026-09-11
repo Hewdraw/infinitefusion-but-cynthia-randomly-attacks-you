af73b634ccc0922c208fcf8acd67f5fa266472ac
Hewdraw <hewdraw@live.nl>
2026-09-11 23:39:30 +0200

    Update 013_UI_Load.rb

--- Data/Scripts/016_UI/013_UI_Load.rb
+++ Data/Scripts/016_UI/013_UI_Load.rb
@@ -300,85 +300,85 @@ class PokemonLoadScreen
     end
   end
 
-  def pbStartLoadScreen
-    copyKeybindings()
+  # def pbStartLoadScreen
+  #   copyKeybindings()
 
-    commands = []
-    cmd_continue     = -1
-    cmd_new_game     = -1
-    cmd_options      = -1
-    cmd_language     = -1
-    cmd_mystery_gift = -1
-    cmd_debug        = -1
-    cmd_quit         = -1
-    show_continue = !@save_data.empty?
-    new_game_plus = show_continue && (@save_data[:player].new_game_plus_unlocked || $DEBUG)
-    if show_continue
-      commands[cmd_continue = commands.length] = _INTL("Continue")
-      # if @save_data[:player].mystery_gift_unlocked
-      #   commands[cmd_mystery_gift = commands.length] = _INTL("Mystery Gift")
-      # end
-    end
-    commands[cmd_new_game = commands.length]  = _INTL("New Game")
-    if new_game_plus
-      commands[cmd_new_game_plus = commands.length]  = _INTL("New Game +")
-    end
-    commands[cmd_options = commands.length]   = _INTL("System Options")
-    commands[cmd_language = commands.length]  = _INTL("Language") if Settings::LANGUAGES[Settings::GAME_ID].length >= 2
-    #commands[cmd_debug = commands.length]     = _INTL("Debug") if $DEBUG
-    commands[cmd_quit = commands.length]      = _INTL("Quit Game")
-    map_id = show_continue ? @save_data[:map_factory].map.map_id : 0
-    @scene.pbStartScene(commands, show_continue, @save_data[:player],
-                        @save_data[:frame_count] || 0, map_id)
-    @scene.pbSetParty(@save_data[:player]) if show_continue
-    @scene.pbStartScene2
-    loop do
-      command = @scene.pbChoose(commands)
-      pbPlayDecisionSE if command != cmd_quit
-      case command
-      when cmd_continue
-        @scene.pbEndScene
-        Game.load(@save_data)
-        return
-      when cmd_new_game
-        @scene.pbEndScene
-        Game.start_new(new_game_plus)
-        echoln new_game_plus
-        @save_data[:player].new_game_plus_unlocked=new_game_plus
-        return
-      when cmd_new_game_plus
-        @scene.pbEndScene
-        Game.start_new(new_game_plus,@save_data[:bag],@save_data[:storage_system],@save_data[:player])
-        @save_data[:player].new_game_plus_unlocked=true
-        return
-      when cmd_mystery_gift
-        pbFadeOutIn { pbDownloadMysteryGift(@save_data[:player]) }
-      when cmd_options
-        pbFadeOutIn do
-          scene = SystemOptionsScene.new
-          screen = PokemonOptionScreen.new(scene)
-          screen.pbStartScreen(true)
-        end
-      when cmd_language
-        @scene.pbEndScene
-        $PokemonSystem.language = pbChooseLanguage
-        pbLoadMessages('Data/' + Settings::LANGUAGES[Settings::GAME_ID][$PokemonSystem.language][1])
-        if show_continue
-          @save_data[:pokemon_system] = $PokemonSystem
-          File.open(SaveData::FILE_PATH, 'wb') { |file| Marshal.dump(@save_data, file) }
-        end
-        $scene = pbCallTitle
-        return
-      when cmd_debug
-        pbFadeOutIn { pbDebugMenu(false) }
-      when cmd_quit
-        pbPlayCloseMenuSE
-        @scene.pbEndScene
-        $scene = nil
-        return
-      else
-        pbPlayBuzzerSE
-      end
-    end
-  end
+  #   commands = []
+  #   cmd_continue     = -1
+  #   cmd_new_game     = -1
+  #   cmd_options      = -1
+  #   cmd_language     = -1
+  #   cmd_mystery_gift = -1
+  #   cmd_debug        = -1
+  #   cmd_quit         = -1
+  #   show_continue = !@save_data.empty?
+  #   new_game_plus = show_continue && (@save_data[:player].new_game_plus_unlocked || $DEBUG)
+  #   if show_continue
+  #     commands[cmd_continue = commands.length] = _INTL("Continue")
+  #     # if @save_data[:player].mystery_gift_unlocked
+  #     #   commands[cmd_mystery_gift = commands.length] = _INTL("Mystery Gift")
+  #     # end
+  #   end
+  #   commands[cmd_new_game = commands.length]  = _INTL("New Game")
+  #   if new_game_plus
+  #     commands[cmd_new_game_plus = commands.length]  = _INTL("New Game +")
+  #   end
+  #   commands[cmd_options = commands.length]   = _INTL("System Options")
+  #   commands[cmd_language = commands.length]  = _INTL("Language") if Settings::LANGUAGES[Settings::GAME_ID].length >= 2
+  #   #commands[cmd_debug = commands.length]     = _INTL("Debug") if $DEBUG
+  #   commands[cmd_quit = commands.length]      = _INTL("Quit Game")
+  #   map_id = show_continue ? @save_data[:map_factory].map.map_id : 0
+  #   @scene.pbStartScene(commands, show_continue, @save_data[:player],
+  #                       @save_data[:frame_count] || 0, map_id)
+  #   @scene.pbSetParty(@save_data[:player]) if show_continue
+  #   @scene.pbStartScene2
+  #   loop do
+  #     command = @scene.pbChoose(commands)
+  #     pbPlayDecisionSE if command != cmd_quit
+  #     case command
+  #     when cmd_continue
+  #       @scene.pbEndScene
+  #       Game.load(@save_data)
+  #       return
+  #     when cmd_new_game
+  #       @scene.pbEndScene
+  #       Game.start_new(new_game_plus)
+  #       echoln new_game_plus
+  #       @save_data[:player].new_game_plus_unlocked=new_game_plus
+  #       return
+  #     when cmd_new_game_plus
+  #       @scene.pbEndScene
+  #       Game.start_new(new_game_plus,@save_data[:bag],@save_data[:storage_system],@save_data[:player])
+  #       @save_data[:player].new_game_plus_unlocked=true
+  #       return
+  #     when cmd_mystery_gift
+  #       pbFadeOutIn { pbDownloadMysteryGift(@save_data[:player]) }
+  #     when cmd_options
+  #       pbFadeOutIn do
+  #         scene = SystemOptionsScene.new
+  #         screen = PokemonOptionScreen.new(scene)
+  #         screen.pbStartScreen(true)
+  #       end
+  #     when cmd_language
+  #       @scene.pbEndScene
+  #       $PokemonSystem.language = pbChooseLanguage
+  #       pbLoadMessages('Data/' + Settings::LANGUAGES[Settings::GAME_ID][$PokemonSystem.language][1])
+  #       if show_continue
+  #         @save_data[:pokemon_system] = $PokemonSystem
+  #         File.open(SaveData::FILE_PATH, 'wb') { |file| Marshal.dump(@save_data, file) }
+  #       end
+  #       $scene = pbCallTitle
+  #       return
+  #     when cmd_debug
+  #       pbFadeOutIn { pbDebugMenu(false) }
+  #     when cmd_quit
+  #       pbPlayCloseMenuSE
+  #       @scene.pbEndScene
+  #       $scene = nil
+  #       return
+  #     else
+  #       pbPlayBuzzerSE
+  #     end
+  #   end
+  # end
 end
