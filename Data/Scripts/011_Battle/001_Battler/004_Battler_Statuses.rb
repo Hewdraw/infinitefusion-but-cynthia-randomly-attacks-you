9a6c38fd0a1644bb443104e0667d45ab2bbebaed
Hewdraw <hewdraw@live.nl>
2026-09-25 00:56:43 +0200

    Update 004_Battler_Statuses.rb

--- Data/Scripts/011_Battle/001_Battler/004_Battler_Statuses.rb
+++ Data/Scripts/011_Battle/001_Battler/004_Battler_Statuses.rb
@@ -251,7 +251,7 @@ class PokeBattle_Battler
       end
     end
     PBDebug.log("[Status change] #{pbThis}'s sleep count is #{newStatusCount}") if newStatus == :SLEEP
-    user.pbRecoverHP(user.hp / 10) if user && !user.fainted? && user.hasActiveEmera?(:VANGUARDBELL)
+    user.pbRecoverHP(user.totalhp / 10) if user && !user.fainted? && user.hasActiveEmera?(:VANGUARDBELL)
     # Form change check
     pbCheckFormOnStatusChange
     # Synchronize
