d041f4105de8d78ffa1e9c782e527f804fd1f0ae
Hewdraw <hewdraw@live.nl>
2026-09-25 18:25:50 +0200

    Update 03_TrainerRematch_System.rb

--- Data/Scripts/053_PIF_Hoenn/TrainerRematches/03_TrainerRematch_System.rb
+++ Data/Scripts/053_PIF_Hoenn/TrainerRematches/03_TrainerRematch_System.rb
@@ -18,7 +18,8 @@ def pbTrainerBattle(trainerID, trainerName,endSpeech=nil,
                     canLose=false, outcomeVar=1,
                     name_override = nil, trainer_type_overide = nil,
                     event_id = nil, map_id = nil)
-  trainer_data = pbLoadTrainer(trainerID, trainerName, trainerPartyID)
+  pbLoadTrainerTemp(trainerID, trainerName, trainerPartyID)
+  trainer_data = GameData::Trainer.get(trainerID,trainerName,trainerPartyID)
   displayPreBattleText(trainer_data)
   map_id = $game_map.map_id
   result = original_pbTrainerBattle(trainerID, trainerName, endSpeech,doubleBattle,trainerPartyID,
@@ -26,6 +27,21 @@ def pbTrainerBattle(trainerID, trainerName,endSpeech=nil,
   updateRematchableTrainer(trainerID, trainerName, trainerPartyID, event_id, nil, map_id) if Settings::GAME_ID == :IF_HOENN
   return result
 end
+
+def pbLoadTrainerTemp(tr_type, tr_name, tr_version = 0)
+  tr_type_data = GameData::TrainerType.try_get(tr_type)
+  raise _INTL("Trainer type {1} does not exist.", tr_type) if !tr_type_data
+  tr_type = tr_type_data.id
+  trainer_data = getTrainersDataMode.try_get(tr_type, tr_name, tr_version)
+  if !trainer_data
+    trainer_data = GameData::Trainer.try_get(tr_type, tr_name, tr_version)
+  end
+  if !trainer_data && [:CHAMPION_Sinnoh, :CREATOR_Minecraft].include?(tr_type) && tr_version > 0 #todo temporary
+    return pbLoadTrainer(tr_type,tr_name,tr_version - 1)
+  end
+  return (trainer_data) ? trainer_data : nil
+end
+
 
 
 def displayPreBattleText(trainer_data)
